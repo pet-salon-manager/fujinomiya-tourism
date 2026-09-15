4af195994ef033d07c14843578342052a9e0decc
pet-salon-manager <sinntyoukoukichild@yahoo.co.jp>
2026-09-16 08:54:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const C='fujinomiya-v1';const A=['./','./index.html','./spots.json','./manifest.webmanifest'];
+const C='fujinomiya-v2';const A=['./','./index.html','./spots.json','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
